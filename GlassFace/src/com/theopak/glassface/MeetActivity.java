6960b2ed1eb257ecf68d906eb19a87f29d8a8668
Theodore X. Pak <mail@theopak.com>
2013-10-05 18:52:18 -0400

    Begins specifics for Glass app.

--- GlassFace/src/com/theopak/glassface/MeetActivity.java
+++ GlassFace/src/com/theopak/glassface/MeetActivity.java
@@ -1,22 +1,79 @@
+/**
+ * MeetActivity.java
+ * AUTH: Theo Pak <mail@theopak.com>
+ * URL:  https://github.com/theopak/glassface
+ */
+
+
 package com.theopak.glassface;
 
-import android.os.Bundle;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TextView;
+import android.widget.Chronometer;
+//import android.os.SystemClock;
+//import android.os.SystemClock;
+//android.speech.SpeechRecognizer
 
+
+/**
+ * StopWatch sample's main activity.
+ */
 public class MeetActivity extends Activity {
+	
+  private TextView mPrompt;
+  private TextView mHint;
+  private Chronometer mClock;
+  private boolean mStarted = false;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_meet);
-	}
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_meet);
+    
+    mClock = (Chronometer) findViewById(R.id.clock);
+    mPrompt = (TextView) findViewById(R.id.prompt);
+    mHint = (TextView) findViewById(R.id.hint);
+  }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.meet, menu);
-		return true;
+  /**
+   * Handle the tap event from the touchpad.
+   */
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    switch (keyCode) {
+    // Handle tap events.
+    case KeyEvent.KEYCODE_DPAD_CENTER:
+    case KeyEvent.KEYCODE_ENTER:
+      toggleSpeech();
+      return true;
+    default:
+      return super.onKeyDown(keyCode, event);
+    }
+  }
+
+  @Override
+  public void onDestroy() {
+	if (mStarted) {
+	  mStarted = false;
 	}
+    super.onDestroy();
+  }
+
+  /**
+   * Toggle the Speech states.
+   */
+  private void toggleSpeech() {
+    if (mStarted) {
+      mPrompt.setText(R.string.prompt_text);
+      mHint.setText(R.string.blank_text);
+    } else {
+      mPrompt.setText(R.string.blank_text);
+      mHint.setText(R.string.hint_text);
+    }
+    mStarted = !mStarted;
+  }
 
 }
