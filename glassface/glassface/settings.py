de310460c7aa5e55bf34bd8b58583b5df0c43a7b
Alex Kau <alex@latenightbyte.com>
2013-10-05 14:50:23 -0400

    fixed derp

--- glassface/glassface/settings.py
+++ glassface/glassface/settings.py
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'glassface.middleware.AuthenticationMiddleware',
+    'glassface.middleware.AuthMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
