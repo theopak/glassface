287db9af93b975c2984bf51400476e8492668a7b
Alex Kau <alex@latenightbyte.com>
2013-10-06 00:13:15 -0400

    fixed static

--- server/glassface/settings.py
+++ server/glassface/settings.py
@@ -90,6 +90,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    PROJECT_PATH+"/static/",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
