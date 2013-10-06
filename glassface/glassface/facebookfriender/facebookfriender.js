67b78254f453827720dd230441957a4837d463a3
Derek Schultz <derek@derekschultz.net>
2013-10-05 20:58:42 -0400

    Various things

--- glassface/glassface/facebookfriender/facebookfriender.js
+++ glassface/glassface/facebookfriender/facebookfriender.js
@@ -2,9 +2,13 @@ var page = require('webpage').create();
 
 var loads = 0;
 var system = require('system');
-var email = system.args[1];
-var pass = system.args[2];
-var fbid = system.args[3];
+var fbid = system.args[1];
+var em = system.args[2];
+var pas = system.args[3];
+
+console.log('email', em);
+console.log('pass', pas);
+console.log('fbid', fbid);
 
 page.onConsoleMessage = function(msg) {
     console.log(msg);
@@ -12,21 +16,26 @@ page.onConsoleMessage = function(msg) {
 
 page.onLoadFinished = function() {
   loads++;
+  console.log("load finished")
   if (loads == 2) {
       page.open("https://www.facebook.com/dialog/friends/?id="+fbid+"&app_id=154878534637314&redirect_uri=http://www.inspiratdesign.com/null.html", function(status){
         page.evaluate(function() {
           document.getElementById('u_0_0').click();
         });
       });
+      page.render("debug.png");
+      console.log("done");
+      phantom.exit();
   };
 };
  
 page.open("https://www.facebook.com/", function(status) {
+  console.log("opened", em);
     if ( status === "success" ) {
-        page.evaluate(function() {
-              document.querySelector("input[name='email']").value = email;
-              document.querySelector("input[name='pass']").value = pass;
-              document.querySelector("#login_form").submit();
-         });
+        page.evaluate(function(em, pas) {
+              document.getElementById("email").value = em;
+              document.getElementById("pass").value = pas;
+              document.getElementById("login_form").submit();
+         }, em, pas);
    }
 });
