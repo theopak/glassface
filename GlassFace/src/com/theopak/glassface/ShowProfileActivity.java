5dd3e899bb1e9855a3336a61861d76fac5714c41
Theodore X. Pak <mail@theopak.com>
2013-10-06 03:59:59 -0400

    Improves user profile view in Android app.

--- GlassFace/src/com/theopak/glassface/ShowProfileActivity.java
+++ GlassFace/src/com/theopak/glassface/ShowProfileActivity.java
@@ -3,21 +3,29 @@ package com.theopak.glassface;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class ShowProfileActivity extends Activity {
+	
+	private TextView mUserName;
+	private TextView mUserTagline;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState); 
         setContentView(R.layout.profile_card);
+        mUserName = (TextView) findViewById(R.id.user_name);
+        mUserTagline = (TextView) findViewById(R.id.user_tagline);
     }
     
     @Override
     public void onStart()
     {
-    	super.onStart();
-        //Refresh data
-        //getDataAndPutToUI();
+    	super.onStart();  // Why is this line necessary !?
+        // Refresh data
+    	mUserName.setText(R.string.default_user_name);
+    	//mUserTagline.setText(R.string.default_user_name);
     }
     
     @Override     
