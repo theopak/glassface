56acf2822d6374dbdd729ed4f95536f3e230a3cd
Alex Kau <alex@latenightbyte.com>
2013-10-06 05:21:41 -0400

    wtf

--- server/glassface/models.py
+++ server/glassface/models.py
@@ -8,15 +8,9 @@ import os
 
 class GlassfaceUser(models.Model):
     user = models.ForeignKey(User)
-<<<<<<< HEAD
-    profile_image = models.FileField(upload_to=get_image_path, blank=True, null=True)
     facebook_email = models.CharField(max_length=255, blank=True, null=True)
     facebook_pass = models.CharField(max_length=255, blank=True, null=True)
     facebook_id = models.CharField(max_length=255, blank=True, null=True)
-=======
     profile_picture = django_filepicker.models.FPFileField(upload_to='uploads')
-    facebook_email = models.CharField(max_length=255)
-    facebook_pass = models.CharField(max_length=255)
->>>>>>> 13844e18863a69fbf5820afd73209b6ce69817f8
 
 admin.site.register(GlassfaceUser)
